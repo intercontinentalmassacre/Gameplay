5c5e697a9d92d5f895e54dda81f45e83e21b3e61
Annenskei <dontneedfriends@gmail.com>
2026-08-12 04:39:13 +0300

    Update ManifestRepository.kt

--- app/src/main/java/app/gamenative/utils/ManifestRepository.kt
+++ app/src/main/java/app/gamenative/utils/ManifestRepository.kt
@@ -14,7 +14,7 @@ import timber.log.Timber
 
 object ManifestRepository {
     private const val ONE_DAY_MS = 24 * 60 * 60 * 1000L
-    private const val MANIFEST_URL = "https://raw.githubusercontent.com/intercontinentalmassacre/Gameplay/refs/heads/master/manifest.json"
+    private const val MANIFEST_URL = "https://raw.githubusercontent.com/intercontinentalmassacre/Gameplay/refs/heads/main/manifest.json"
     private val json = Json { ignoreUnknownKeys = true }
 
     suspend fun loadManifest(context: Context): ManifestData {
